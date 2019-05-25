59044331e2d45d490c27915713e910e03dc2764c
Richard <richard.trow.davis@gmail.com>
2019-05-24 20:37:06 -0700

    Adding section headers for environment variables, app listener, and connecting to mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,26 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const db = mongoose.connection;
 const app = express();
-const PORT = process.env.PORT || 5000;
 
 // Routes
 app.get('/', (req, res) => {
   res.send('root route')
 });
 
-// Starting Server
-app.listen(PORT, (req, res) => {
-  console.log(`Server is listening on port: ${PORT}`);
-})
+//-------------------------//
+//  Environment Variables  //
+//-------------------------//
+const port = process.env.PORT || 5000;
+// const MONGODB_URI = process.env.MONGODB_URI
+
+//---------------------------//
+//  App Listener: Port 5000  //
+//---------------------------//
+app.listen(port, () => {
+  console.log(`Server is listening on port: ${port}`);
+});
+
+//------------------------//
+//  Connect to Mongooose  //
+//------------------------//
