62f8b83d09dcee07222d0b29e9bbb1d3b336d16a
Richard <richard.trow.davis@gmail.com>
2019-07-03 21:45:55 -0700

    Fully separated out express app into app.js and server connection into server.js. Added sections to each piece of code in app.js and server.js

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,23 @@
+//----------------//
+//  Dependencies  //
+//----------------//
 const express = require('express');
 const app = express();
+const methodOverride = require('method-override');
+
+//--------------//
+//  Middleware  //
+//--------------//
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static('public'))
+app.use(methodOverride('_method'));
+
+//------------------------//
+//  Customers Controller  //
+//------------------------//
+const customersController = require('./controllers/customers.js');
+app.use('/customers', customersController);
 
 //----------//
 //  Routes  //
--- backend/server.js
+++ backend/server.js
@@ -1,70 +1,34 @@
+//----------------//
 //  Dependencies  //
 //----------------//
-const express = require('express');
-const app = express();
+const app = require('./app.js');
 const mongoose = require('mongoose');
 const db = mongoose.connection;
-const methodOverride = require('method-override');
 
-//--------------//
-//  Middleware  //
-//--------------//
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(express.static('public'))
-app.use(methodOverride('_method'));
-
-//  Routes  //
-//----------//
-app.get('/', (req, res) => {
-  res.send('Root Route Works!')
-});
-
-app.get('/users', (req, res) => {
-  res.send('You have reached the users page.')
-})
-
-//  Customers Controller  //
-//------------------------//
-const customersController = require('./controllers/customers.js');
-app.use('/customers', customersController);
-
-
+//-------------------------//
 //  Environment Variables  //
 //-------------------------//
 const port = process.env.PORT || 5000;
 const database = 'mug_club';
 const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/' + database
 
+//---------------------------//
 //  App Listener: Port 5000  //
 //---------------------------//
 app.listen(port, () => {
   console.log(`Server is listening on port: ${port}`);
 });
 
-// //  Connect to Mongooose  //
-// //------------------------//
+//------------------------//
+//  Connect to Mongooose  //
+//------------------------//
 mongoose.connect(mongoURI, { useNewUrlParser: true }, () => {
   console.log('The connection with mongod is established');
 })
 
+//-----------------//
 //  Error Messages //
 //-----------------//
 db.on('Error!', (err) => console.log(err.message + 'Is mongod not running?'))
 db.on('Connected', () => console.log('Mongo connected: ', mongoURI))
 db.on('Disconnected', () => console.log('Mongo Disconnected'))
-
-
-// //Test Section
-// const firstUser = {
-//   FirstName: 'Jacob',
-//   LastName: 'Burger'
-// };
-//
-// User.create(firstUser, (error, user) => {
-//   if(error) {
-//     console.log(error);
-//   } else {
-//     console.log(user);
-//   }
-// })
