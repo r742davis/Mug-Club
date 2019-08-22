1295df6a1b8df1eba5ef552225d1127addb371aa
Richard <richard.trow.davis@gmail.com>
2019-08-21 23:50:51 -0400

    Added css to Customer.js

--- client/src/Components/Customer/Customer.js
+++ client/src/Components/Customer/Customer.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
+import classes from './Customer.module.css';
+
 const customer = (props) => {
   return (
-    <div>
+    <div className={classes.CustomerBox}>
       <h3> Hello my name is {props.firstName} {props.lastName}! </h3>
     </div>
   )
