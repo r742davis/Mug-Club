f804a03f069b8e5f0de7b9a0548ce8821f9971a0
Richard Davis <richard.trow.davis@gmail.com>
2019-11-03 12:01:43 -0500

    Added new elements to customer database in the customer.js schema. Changed corresponding values in customers.js controller. Tested in Postman - Test was successful in adding both new elements as well as nested elements for the mugClub and beer objects. Started working on update route for customers.js controller.

--- backend/controllers/Customers.js
+++ backend/controllers/Customers.js
@@ -18,7 +18,22 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
     const newCustomer = new Customer({
       firstName: req.body.firstName,
-      lastName: req.body.lastName
+      lastName: req.body.lastName,
+      username: req.body.username,
+      password: req.body.password,
+      email: req.body.email,
+      mugClub: {
+        completed: req.body.mugClub.completed,
+        clubId: req.body.mugClub.clubId,
+        beers: {
+          coors: req.body.mugClub.beers.coors,
+          coorsLight: req.body.mugClub.beers.coorsLight,
+          budweiser: req.body.mugClub.beers.budweiser,
+          budLight: req.body.mugClub.beers.budLight,
+          sierraNevadaPaleAle: req.body.mugClub.beers.sierraNevadaPaleAle,
+          sierraNevadaTorpedo: req.body.mugClub.beers.sierraNevadaTorpedo
+        }
+      }
     })
 
     newCustomer.save().then(customer => res.json(customer))
@@ -36,6 +51,16 @@ router.delete('/:id', (req, res) => {
 //SHOW route
 
 //UPDATE route
+router.put('/:id', async (req, res) => {
+  try {
+    const updateCustomer = await Customer.findByIdAndUpdate(req.params.id, req.body, {new:true});
+    res.status(200).json(updateCustomer);
+  } catch (error) {
+    res.status(400).json({error: error.message});
+  }
+
+})
+
 
 module.exports = router;
 
--- backend/models/customer.js
+++ backend/models/customer.js
@@ -2,8 +2,40 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema
 
 const customerSchema = new Schema({
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true }
+  firstName: {
+    type: String,
+    required: [true, 'Yo, give me your first name']
+  },
+  lastName: {
+    type: String,
+    required: [true, 'Dude, you don\'t have a last name?']
+  },
+  id: Schema.Types.ObjectId,
+  username: {
+    type: String,
+    required: [true, 'Username is required']
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required']
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required']
+  },
+  mugClub: {
+    completed: Boolean,
+    cludId: Number,
+    beers: {
+      coors: Boolean,
+      coorsLight: Boolean,
+      budweiser: Boolean,
+      budLight: Boolean,
+      sierraNevadaPaleAle: Boolean,
+      sierraNevadaTorpedo: Boolean
+    }
+  }
+
 }, {timestamps: true});
 
 const Customer = mongoose.model('Customer', customerSchema)
