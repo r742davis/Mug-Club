0149c7b816ab7bf3210de08cdda842c02886f629
Richard Davis <richard.trow.davis@gmail.com>
2019-12-19 13:39:28 -0500

    Fixed issue with exporting the TypeMap variable. Works perfectly now

--- client/src/Components/Beer/TypeMap.js
+++ client/src/Components/Beer/TypeMap.js
@@ -2,16 +2,13 @@ import React from 'react';
 import beerTypes from './BeerTypes';
 const uniqid = require('uniqid');
 
-const typeMap = () => {
-  Object.entries(beerTypes)
-    .map(([key, value]) => {
-        return <optgroup key={uniqid()} label={key.split(/(?=[A-Z])/).join(" ")}>{
-          value.map(type => <option key={uniqid()} value={type}>{type}</option>)
-        }</optgroup>
-      }
-    );
-}
+export const typeMap = Object.entries(beerTypes)
+  .map(([key, value]) => {
+      return <optgroup key={uniqid()} label={key.split(/(?=[A-Z])/).join(" ")}>{
+        value.map(type => <option key={uniqid()} value={type}>{type}</option>)
+      }</optgroup>
+    }
+  );
 
-export default typeMap
 
 //Iterate over object, return the optgroup with the key name, then map each key's array to display the options within that optgroup
