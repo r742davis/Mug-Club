ba01c32c847ff577664701f7dc0e66d70c745010
Richard Davis <richard.trow.davis@gmail.com>
2020-02-05 23:35:36 -0500

    Redesigned Search page to match login page

--- client/src/components/Customer.js
+++ client/src/components/Customer.js
@@ -4,29 +4,25 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 // import Button from '@material-ui/core/Button';
 // import Typography from '@material-ui/core/Typography';
-// import BeersList from './BeersList';
+import BeersList from './BeersList';
 import classes from './styles/Customer.module.css';
 
 const customer = (props) => {
   return (
     <div className={classes.CustomerBox}>
-      <Card>
-      <CardContent>
         <h1>{props.name.first} {props.name.last}</h1>
         <h2>Mug Club ID: {props.clubId}</h2>
         <h2>Completed?</h2>
-        
-        {/* <Button onClick={props.handleDisplayBeer}>Display Beer List</Button>
-        { props.displayBeer ?
+        <BeersList beers={props.beers} />
+        {/* <button onClick={props.handleDisplayBeer}>Display Beer List</button> */}
+        {/* { props.displayBeer ?
           <BeersList beers={props.beers} /> : null } */}
-      </CardContent>
-      <button 
-        className={classes.EditButton} 
-        onClick={props.toggleEditCustomerModal}><span className={classes.Span}>Edit</span></button>
-      <button 
-        className={classes.DeleteButton}
-        onClick={props.deleteCustomer}><span className={classes.Span}>Delete</span></button>
-      </Card>
+        <button 
+          className={classes.EditButton} 
+          onClick={props.toggleEditCustomerModal}><span className={classes.Span}>Edit</span></button>
+        <button 
+          className={classes.DeleteButton}
+          onClick={props.deleteCustomer}><span className={classes.Span}>Delete</span></button>
     </div>
   )
 }
