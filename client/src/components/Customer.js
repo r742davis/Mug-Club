fb72fe35b1b2e36bd4b51b66318b7d369b2a6f75
Richard Davis <richard.trow.davis@gmail.com>
2020-03-20 13:57:15 -0400

    Redesigned customer display cards so that they are less busy. Changed application font to default as Roboto. Altered Customer.js component to display more info in Title card for customer

--- client/src/components/Customer.js
+++ client/src/components/Customer.js
@@ -3,32 +3,42 @@ import classes from './styles/Customer.module.css';
 
 const Customer = (props) => {
   return (
-    <div className={classes.CustomerBox}>
-      <h1 className={props.completed ? classes.CompletedTitle : classes.CustomerBoxTitle}>{props.name.first} {props.name.last}
-        <span className={classes.ButtonContainer}>
-          <button 
-            className={classes.EditButton} 
-            onClick={props.toggleEditCustomerModal}>
-            Edit
-          </button>
-          <button 
-            className={classes.DeleteButton}
-            onClick={props.deleteCustomer}>
-            Delete
-          </button>
-        </span>
-      </h1>
-      <span className={classes.Span}>
-        <h2>Mug Club ID:</h2>
-        <h2>{props.clubId}</h2>
-      </span>
-      <span className={classes.Span}>
+    <div className={classes.CustomerCard}>
+
+      <div className={
+        props.completed ? 
+        classes.CompletedTitleContainer : 
+        classes.IncompleteTitleContainer
+        }>
+        
+        <h1 className={classes.CustomerTitle}>{props.name.first} {props.name.last}
+          
+          <span className={classes.ButtonContainer}>
+            <button 
+              className={classes.EditButton} 
+              onClick={props.toggleEditCustomerModal}>
+              Edit
+            </button>
+            <button 
+              className={classes.DeleteButton}
+              onClick={props.deleteCustomer}>
+              Delete
+            </button>
+          </span>
+
+          
+        </h1>
+        <h2 className={classes.CustomerDetails}>Mug Club #{props.clubId}</h2>
+      {/* <h2 className={classes.CustomerDetails}>Beer{props.calculateCompletedBeers(props.beers)} </h2> */}
+      </div>
+      
+      {/* <span className={classes.Span}>
         <h2>Completed?</h2>
         <h2>{props.completed ? 'Completed!' : 'Not Yet!'}</h2>
-      </span>
+      </span> */}
       <span className={classes.Span}>
-        <h2>Beers Completed:</h2>
-        <h2>{props.calculateCompletedBeers(props.beers)}</h2>
+    <h2 className={classes.CustomerDetails}>Beers Completed: {props.calculateCompletedBeers(props.beers)} / {props.beers.length}</h2>
+        <h2></h2>
       </span>
     </div>
   )
