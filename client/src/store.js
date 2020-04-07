ae3f113ae8cafad3dea0821d2d898817dd2dd187
Richard Davis <richard.trow.davis@gmail.com>
2020-04-07 11:56:54 -0400

    Commit from heroku-deploy.py for deployment

--- client/src/store.js
+++ client/src/store.js
@@ -6,13 +6,26 @@ const initialState = {};
 
 const middleware = [thunk];
 
-const store = createStore(
-  rootReducer,
-  initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+if (process.env.NODE.ENV === 'production') {
+  const store = createStore(
+    rootReducer,
+    initialState,
+    compose(
+      applyMiddleware(...middleware)
+    )
   )
-);
+} else {
+  const store = createStore(
+    rootReducer,
+    initialState,
+    compose(
+      applyMiddleware(...middleware),
+      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+  );
+}
+
+
+
 
 export default store;
