df23617a19f7f78d2423fae828f9e367762db46d
Richard Davis <richard.trow.davis@gmail.com>
2020-04-07 16:29:58 -0400

    Commit from heroku-deploy.py for deployment

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,12 @@ app.use(
 );
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, '/build')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, '/build')));
+} else {
+  app.use(express.static(path.join(__dirname, '/client/public/index.html')));
+}
+
 app.use(methodOverride("_method"));
 
 //  Customers Controller  //
