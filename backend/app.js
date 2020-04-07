7c06ce8df79fbd18888658943d13ab53653f8689
Richard Davis <richard.trow.davis@gmail.com>
2020-04-06 23:30:32 -0400

    Commit from heroku-deploy.py for deployment

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ app.use(
 );
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'build')));
+// app.use(express.static(path.join(__dirname, 'build')));
 app.use(methodOverride("_method"));
 
 //  Customers Controller  //
